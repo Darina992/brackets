2a9a97b5fb57aa7196ed31e2a969420b1a7c0acc
Darina992 <darina992@gmail.com>
2022-02-01 12:44:20 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
   let obj = {};
-let stack = [];
-for(let i = 0; i < bracketsConfig.length; i++){
-  obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
-}
-for(let i = 0; i <str.length; i++){
-  let current = str[i];
-  if(Object.keys(obj).indexOf(current) !== -1){
-    stack.push(current);
-    console.log(stack)
-  } else {
-    if(stack.length === 0){
-      return false
-    } else {
+  let stack = [];
+  for(let i = 0; i < bracketsConfig.length; i++){
+    obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  //console.log(obj)
+  for(let i = 0; i <str.length; i++){
+    let current = str[i];
+    //console.log(current);
+    if (current === '|') {
       let top = stack[stack.length-1];
-      console.log(top);
-      for(let key in obj){
-        if(top === key){
-          if(current === obj[key]){
-            stack.pop(top)
+      if (top !== current){
+        stack.push(current);
+        console.log(current,top)
+      } else {
+        stack.pop(top);
+      }
+    } else if (Object.keys(obj).indexOf(current) !== -1){
+      stack.push(current);
+      //console.log(stack);
+    } else {
+      if(stack.length === 0){
+        return false
+      } else {
+        let top = stack[stack.length-1];
+        //console.log(top);
+        for(let key in obj){
+          if(top === key){
+            if(current === obj[key]){
+              stack.pop(top)
+            }
           }
         }
       }
+
     }
 
   }
+   return (stack.length === 0)
+
 
 }
  return (stack.length === 0)
